b0831941ee02eabf2a896c597650fa34db4b95fb
PALLANAVARATNALU <134999557+PALLANAVARATNALU@users.noreply.github.com>
2025-05-02 12:41:39 +0530

    Add files via upload

---
+++ practicing1.py
@@ -0,0 +1,20 @@
+# SWAPPING A NUMBERS WITH 2 VARIABLES AND 3RD VARAIBLE
+
+# 2 VARIABLES
+
+x=10
+y=20
+x,y=y,x
+print(x)
+print(y)
+
+
+#  using 3rd varaible
+x=10
+y=20
+z=x
+x=y
+y=z
+print(x)
+print(y)
+
